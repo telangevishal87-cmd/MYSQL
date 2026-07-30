b478611a992f8f02dda7c5cec6fb5a034e66ec5b
vishal <telange vishal>
2026-07-30 13:05:57 +0530

    create a table

--- db_banking.sql
+++ db_banking.sql
@@ -1,15 +1,22 @@
--- first command
-create database banking_DB1;
--- 2nd command
-use  banking_DB1;
--- 3rd command
-create table Account_holder_Date(
-Account_holder_name  varchar (100) not null,
-Account_number int not null,
-Email varchar(200),
-Age tinyint not null
+create database practice3;
+
+use practice3;
+
+create table practice3_table(
+customer_id int primary key,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+email varchar(100) unique not null,
+phone_number varchar(20)
 );
 
-select * from Account_holder_Date;
+select*from practice3_table;
+
+insert into practice3_table (customer_id,first_name,last_name,email,phone_number)
+value ( 1001,"vishal","telange","telangevishal05@gmail.com","9823465698"),
+(1002,"ashok","padature","ashokpadature89@gmail.com","8723457609"),
+(1003,"dipak","giri","dipakgiri65@gmail.com","9503123670");
+
+ 
 
 
