5969adcba63f1112eeaf04b7f13d4430559c31e2
vishal <telange vishal>
2026-07-31 13:02:28 +0530

    alter in add column and constraint and drop

--- db_banking.sql
+++ db_banking.sql
@@ -17,6 +17,37 @@ value ( 1001,"vishal","telange","telangevishal05@gmail.com","9823465698"),
 (1002,"ashok","padature","ashokpadature89@gmail.com","8723457609"),
 (1003,"dipak","giri","dipakgiri65@gmail.com","9503123670");
 
+select * from practice3_table;
+
+CREATE TABLE Accounts (
+    AccountID INT,
+    AccountType VARCHAR(20),
+    Balance DECIMAL(10,2)
+);
+
+select * from Accounts;
+select AccountID from Accounts;
+
+CREATE TABLE Transactions (
+    TransactionID INT,
+    TransactionDate DATE,
+    Amount DECIMAL(10,2),
+    TransactionType VARCHAR(20)
+);
+select * from Transactions;
+CREATE TABLE Branches (
+    BranchID INT,
+    BranchName VARCHAR(100),
+    BranchAddress VARCHAR(200),
+    BranchPhone VARCHAR(15)
+);
+
+select * from Branches;
+ 
+ ALTER TABLE Accounts
+ADD CONSTRAINT chk_MinBalance
+CHECK (Balance >= 1000);
+
  
 
 
